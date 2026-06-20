1b6abf249a5c282c9cb33df7b3df138d03d7fdf7
Ana Paula Araujo <paulinha120595@gmail.com>
2026-06-20 17:47:38 -0300

    feat: Estrutura inicial do projeto

---
+++ writer-tool-box/src/app/count-caracter/count-caracter.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CountCaracter } from './count-caracter';
+
+describe('CountCaracter', () => {
+  let component: CountCaracter;
+  let fixture: ComponentFixture<CountCaracter>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [CountCaracter],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(CountCaracter);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ writer-tool-box/src/app/count-caracter/count-caracter.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-count-caracter',
+  imports: [],
+  templateUrl: './count-caracter.html',
+  styleUrl: './count-caracter.scss',
+})
+export class CountCaracter {}
